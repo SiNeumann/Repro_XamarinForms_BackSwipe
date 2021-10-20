1703b53843fb9497ba53b82001568912090a4922
Siegfried Neumann <SieNeu00@omicron.at>
2021-10-20 08:48:53 +0200

    repro + vide

---
+++ BackSwipe/BackSwipe/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BackSwipe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void Next_Clicked(object sender, EventArgs e)
+        {
+            var page = new PageWithNavigationBar();
+            await Navigation.PushAsync(page);
+        }
+    }
+}
---
+++ BackSwipe/BackSwipe/PageWithNavigationBar.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BackSwipe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageWithNavigationBar : ContentPage
+    {
+        public PageWithNavigationBar()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, true);
+        }
+    }
+}
